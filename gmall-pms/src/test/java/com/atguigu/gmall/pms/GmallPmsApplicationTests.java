4edb15391cdf78dcaad018ca5834e77603e360e8
boabo <2532509192@qq.com>
2019-05-09 16:40:52 +0800

    项目结构创建完成11111

--- gmall-pms/src/test/java/com/atguigu/gmall/pms/GmallPmsApplicationTests.java
+++ gmall-pms/src/test/java/com/atguigu/gmall/pms/GmallPmsApplicationTests.java
@@ -1,7 +1,12 @@
 package com.atguigu.gmall.pms;
 
+import com.atguigu.gmall.pms.entity.Brand;
+import com.atguigu.gmall.pms.entity.Product;
+import com.atguigu.gmall.pms.service.BrandService;
+import com.atguigu.gmall.pms.service.ProductService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +14,23 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class GmallPmsApplicationTests {
 
+	@Autowired
+	ProductService productService;
+	@Autowired
+	BrandService brandService;
 	@Test
 	public void contextLoads() {
+//		Product byId = productService.getById(1);
+//		System.out.println(byId.getName());
+//
+//		Brand brand = new Brand();
+//		brand.setName("hhhhhh");
+//		brandService.save(brand);
+//		System.out.println("ssssssssss");
+		Brand byId = brandService.getById(53);
+		System.out.println(byId.getName());
+
+
 	}
 
 }
